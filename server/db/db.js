73c362f176a19d4f0bc8a6091b5cf52bddb57488
student <student@pop-os.localdomain>
2023-10-27 12:42:12 +1300

    added routes

--- server/db/db.js
+++ server/db/db.js
@@ -5,10 +5,10 @@ const environment = process.env.NODE_ENV || 'development'
 const config = knexfile[environment]
 export const connection = knex(config)
 
-export async function getUsers() {
-  return connection('users').select()
+export async function getOwners() {
+  return connection('owners').select()
 }
 
-export async function getUser(id) {
-  return connection('users').where('id', id).first()
-}
+// export async function getUser(id) {
+//   return connection('users').where('id', id).first()
+// }
--- server/routes/users.js
+++ server/routes/users.js
@@ -6,11 +6,19 @@ const router = express.Router()
 
 router.get('/', async (req, res) => {
   try {
-    const users = await db.getUsers()
-    res.render('home', { users: users })
+    // const users = await db.getUsers()
+    res.render('home')
   } catch (err) {
     res.status(500).send('DATABASE ERROR: ' + err.message)
   }
 })
 
+router.get('/owners', async (req, res) => {
+  const owners = await db.getOwners()
+  res.render('owners', owners)
+})
+
+router.get('/:id', async (req, res) => {
+  
+})
 export default router
